31e2317c7f39ac83728d1ddb5ed36bbef18fa775
zhangyx1262 <1650408632@qq.com>
2019-10-23 09:21:07 +0800

    删除node_modules

--- sps-fe/node_modules/bee-radio/src/index.js
+++
@@ -1,8 +0,0 @@
-import RadioGroup from './RadioGroup.js';
-import Radio from './Radio.js';
-import RadioButton from './RadioButton.js';
-
-Radio.RadioGroup = RadioGroup;
-Radio.RadioButton = RadioButton;
-
-export default Radio;
